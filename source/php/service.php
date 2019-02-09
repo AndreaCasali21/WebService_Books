7b62e40e6aeeff48dce17596274e3863d3041d68
MarcoCicognani <marco.cicognani@btstore.com>
2019-02-09 09:52:12 +0100

    going on with parser implementation

--- source/php/service.php
+++ source/php/service.php
@@ -10,18 +10,29 @@ const TYPE_CMP = 2;
 
 // the index of the command on the query
 const CMD_IDX = 0;
+const TABLE_IDX = 0;
+const PROP_IDX = 1;
 
 // the expressions types
 const EXPR_VAL = 0;
 const EXPR_PROP = 1;
 
+// token and syntax identifiers
+const TOKEN_WORK_SEPRT  = ' ';
+const TOKEN_PROP_ACCESS = '.';
+const TOKEN_COUNT = 'COUNT';
+const TOKEN_AND   = 'AND';
+const TOKEN_OR    = 'OR';
+const TOKEN_IS    = 'IS';
+const TOKEN_NIS   = 'NOT_IS';
+
 // the list of tokens avaible for this language with
 // the list of avaible commands supported by the language
 $TOKENS = array(
     // counts the evaluated query
-    'COUNT' => {
+    TOKEN_COUNT => {
         // the action to perform with this command
-        'action' => function ($param) {
+        'action' => function ($params) {
 
         },
         // the type of the token
@@ -30,9 +41,9 @@ $TOKENS = array(
         'params' => 1
     },
     // connects two expessions
-    'AND' => {
+    TOKEN_AND => {
         // the action to perform with this command
-        'action' => function ($first, $second) {
+        'action' => function ($params) {
 
         }
         // the type of the token
@@ -42,9 +53,9 @@ $TOKENS = array(
         'params' => 2
     },
     // connects two expessions
-    'OR' => {
+    TOKEN_OR => {
         // the action to perform with this command
-        'action' => function ($first, $second) {
+        'action' => function ($params) {
 
         }
         // the type of the token
@@ -54,9 +65,9 @@ $TOKENS = array(
         'params' => 2
     },
     // compares two expessions
-    'IS' => {
+    TOKEN_IS => {
         // the action to perform with this command
-        'action' => function ($first, $second) {
+        'action' => function ($params) {
 
         }
         // the type of the token
@@ -65,10 +76,15 @@ $TOKENS = array(
         'params' => 2
     },
     // compares two expessions
-    'NOT_IS' => {
+    TOKEN_NIS => {
         // the action to perform with this command
-        'action' => function ($first, $second) {
+        'action' => function ($params) {
+            // split the two paramters
+            $exprleft = $params[0];
+            $exprright = $params[1];
 
+            // now we have the tables
+            
         }
         // the type of the token
         'type' => TYPE_CMP,
@@ -76,6 +92,13 @@ $TOKENS = array(
         'params' => 2
     }
 );
+
+/**
+ * Checks whether the <$str> string contains the <$needle> one
+ */
+function str_contains(string $str, string $needle) {
+    return ( strpos($str, $needle) !== false );
+}
 
 /**
  * Retrivies the content of the file provided by the path
@@ -94,27 +117,33 @@ function load_db() {
  * Parses the provided query as MRQL (MeetiX Reduced Query Language)
  */
 function parse_query(string $query) {
+    // check whether the separator token and the table access token are valid
+    if (TOKEN_WORK_SEPRT != ' ' || TOKEN_WORK_SEPRT == TOKEN_PROP_ACCESS) {
+        $ERROR_MSG = 'bad configured token for separator and table access';
+        return false;
+    }
+
     // all the tokens are putted on this array
     $tokens = array();
 
     // so now split the string in a list of tokens
     // that are subsequently interpreted by a very
-    // basic check alghorithm
-    $token = strtok($query, ' ');
-    while ($token !== false) {
-        array_push($tokens, $token);
-        $token = strtok($query);
+    // basic check alghorithm, each token before the
+    // insertion on the <tokens> array is trimmed of
+    // any useless characters
+    {
+        $token = strtok($query, ' ');
+        while ($token !== false) {
+            array_push($tokens, trim($token));
+            $token = strtok($query);
+        }
     }
-
-    // now each token must be trimmed
-    foreach ($tokens as &$token)
-        $token = trim($token);
 
     // now parse each tooken to get the right action to do
     // the first token must be the command, so search it
     // into the $COMMANDS array of functions
-    if (!array_key_exists($token[CMD_IDX], $TOKENS) || $TOKENS[$token[CMD_IDX]]['type'] != TYPE_CMD) {
-        $ERROR_MSG = "query command not supported";
+    if (!array_key_exists($tokens[CMD_IDX], $TOKENS) || $TOKENS[$tokens[CMD_IDX]]['type'] != TYPE_CMD) {
+        $ERROR_MSG = "query command not supported or unexisting";
         return false;
     }
 
@@ -124,40 +153,50 @@ function parse_query(string $query) {
     // with the token descriptor because this is
     // not necessary
     $expressions = array();
-    $actions = array($TOKENS[$token[CMD_IDX]]);
+    $actions = array($TOKENS[$tokens[CMD_IDX]]);
 
     // the following tokens must be tokens, parameters,
     // expessions and so on, so search them on the TOKENS
     // array and get the actions
     foreach (next($tokens) as $token) {
         // if the token does not exists on the tokens array
-        // we take it as expession, commonly <table.property> or <value>
+        // we take it as expession, commonly the parser expected
+        // <table<TOKEN_PROP_ACCESS>property> or <value>
         if (!array_key_exists($token, $TOKENS)) {
-            // check whether the expression is a <table.property> or <value>
+            // check whether the expression is a <table.property> or
+            // <value>,  for now the discriminant beetween the two
+            // expressions types is the <TOKEN_PROP_ACCESS> character(s)
             $type = EXPR_PROP;
-            if (strpos($token, '.') !== false) $type = EXPR_VAL;
+            if (str_contains($token, TOKEN_PROP_ACCESS))
+                $type = EXPR_VAL;
 
             // put the value on the array
-            array_push($expressions, array('string' => $token, 'type' => $type));
+            array_push($expressions, array(
+                'expr' => $token,
+                'type' => $type
+            ));
         }
 
         // collect all the parameters, NOTE this is a
         // very orrible solutions, but for now works
-        $tkparams = array();
+        $params = array();
         $action = $TOKENS[$token];
-        if ($action['params'] == 1) {
-            array_push($tkparams, &next($tokens));
-            prev($tokens);
-        }
-        else if ($action['params'] == 2) {
-            array_push($tkparams, &prev($tokens));
-            next($tokens);
-            array_push($tkparams, &next($tokens));
-            prev($tokens);
+        switch ($action['params']) {
+            case 1:
+                array_push($params, &next($tokens));
+                prev($tokens);
+                break;
+            case 2:
+                array_push($params, &prev($tokens));
+                next($tokens);
+                array_push($params, &next($tokens));
+                prev($tokens);
+                break;
         }
 
         // put on the actions array the provided actions
-        array_push($actions, array_merge($action, array('params' => $tkparams));
+        $action['params'] = $params;
+        array_push($actions, $action);
     }
 
     // so now is time to evaluate and execute properly the query
@@ -168,8 +207,8 @@ function parse_query(string $query) {
         if ($expression['type'] == EXPR_PROP) {
             // explode all the expression, that commonly
             // is expected is a <table.property>
-            $exprstr = $expression['string'];
-            $parts = explode('.', $exprstr);
+            $exprstr = $expression['expr'];
+            $parts = explode(TOKEN_PROP_ACCESS, $exprstr);
 
             // check whether the table exists
             if (!count($parts) || count($parts) < 2) {
@@ -178,21 +217,27 @@ function parse_query(string $query) {
             }
 
             // check whether the table exists
-            if (!array_key_exists($parts[0], $DB_DATA)) {
+            if (!array_key_exists($parts[TABLE_IDX], $DB_DATA)) {
                 $ERROR_MSG = "requested an unexisting table on the DB";
                 return false;
             }
 
             // check whether the property exists on the table
-            if (!array_key_exists($parts[1], $DB_DATA[$parts[0]][0])) {
+            if (!array_key_exists($parts[PROP_IDX], $DB_DATA[$parts[TABLE_IDX]][0])) {
                 $ERROR_MSG = "requested an unexisting table property";
                 return false;
             }
 
             // so we can load from the db the values requested
             $values = array();
-            foreach ($DB_DATA[$parts[0]] as $tuple)
-                array_push($values, $tuple[$parts[1]]);
+            foreach ($DB_DATA[$parts[TABLE_IDX]] as $tuple) {
+                // put on the values array the entire tuple that is needed
+                // later for eventual selection, and the value to check
+                array_push($values, array(
+                    'tuple' => $tuple,
+                    'value' => $tuple[$parts[PROP_IDX]];
+                ));
+            }
 
             // now put on the expression the values loaded
             $expression['values'] = $values;
